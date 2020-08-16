88326bb64b61b8b4accb3e225861e26aa4251793
Nemanja Miladinovic <nemanjaks4@gmail.com>
2020-08-16 20:02:48 +0200

    Added SolrEngine

---
+++ SolrEngine/Lyrics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SolrNet.Attributes;
+
+namespace SolrEngine
+{
+    internal class Lyrics
+    {
+        [SolrUniqueKey("id")]
+        public string Id { get; set; }
+
+        [SolrField("singer")]
+        public string Singer { get; set; }
+        [SolrField("title")]
+        public string Title { get; set; }
+        [SolrField("lyric")]
+        public string Lyric { get; set; }
+    }
+}
